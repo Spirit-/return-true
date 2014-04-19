5de5fe51683dcfe3191c3c3d06e3fb6bffe0ac99
Spirit- <pbojemski@gmail.com>
2014-04-19 21:37:37 +0200

    Methoda sprawdzająca czy kategoria ma rodzica

--- application/classes/Controller/Forum.php
+++ application/classes/Controller/Forum.php
@@ -10,6 +10,6 @@ class Controller_Forum extends Controller_Base
     public function action_index()
     {
         $categories = ORM::factory('Category')->find_all();
-	$this->template->content= View::factory('forum/index')->bind('categories', $categories);
+    	$this->template->content= View::factory('forum/index')->bind('categories', $categories);
     }
 } 
